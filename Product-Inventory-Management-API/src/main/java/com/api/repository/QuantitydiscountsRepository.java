ecf3479a7be2e29dab621af27872da361d45adac
AarshaDasKM <aarshadaskm@gmail.com>
2025-04-30 17:29:23 +0530

    "Update"

--- Product-Inventory-Management-API/src/main/java/com/api/repository/QuantitydiscountsRepository.java
+++ Product-Inventory-Management-API/src/main/java/com/api/repository/QuantitydiscountsRepository.java
@@ -1,10 +1,14 @@
 package com.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.entity.Products;
 import com.api.entity.Quantitydiscounts;
 
 public interface QuantitydiscountsRepository extends JpaRepository<Quantitydiscounts, Integer> {
+
+	Optional<Quantitydiscounts> findByMinQuantity(int quantity);
 
 }
--- Product-Inventory-Management-API/src/main/java/com/api/service/ProductServiceImpl.java
+++ Product-Inventory-Management-API/src/main/java/com/api/service/ProductServiceImpl.java
@@ -11,13 +11,15 @@ import org.springframework.stereotype.Service;
 import com.api.entity.Products;
 import com.api.entity.Quantitydiscounts;
 import com.api.repository.ProductRepository;
+import com.api.repository.QuantitydiscountsRepository;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
 	@Autowired
 	ProductRepository productRepository;
-	
+	@Autowired
+	QuantitydiscountsRepository quantitydiscountsRepository;
 	@Override
 	public List priceCalculation(String productId, int quantity, String promoCode, String userType) {
     Optional<Products> product=productRepository.findById(productId);
@@ -26,7 +28,11 @@ public class ProductServiceImpl implements ProductService {
 	 List result=new LinkedList<>();
 	 result.add(productId);
 	 result.add(originalPrice);
-	
+	 Optional<Quantitydiscounts> quantitydiscount=quantitydiscountsRepository.findByMinQuantity(quantity);
+	 Quantitydiscounts originalquant=quantitydiscount.get();
+		Float discount =originalquant.getDiscountPercentage();
+	 float finalPrice=originalPrice-(originalPrice*discount)/100;
+	 result.add(finalPrice);
 		return result;
 	}
 
